52ff8b9a1059be0c46b7bd3ba0856bb6d093e834
easonnp <145891284+easonnp@users.noreply.github.com>
2023-10-12 12:05:31 +0800

    upload all files

---
+++ vegalite_charts.js
@@ -0,0 +1,24 @@
+var choropleth_map = "./choropleth_map.vg.json";
+vegaEmbed('#choropleth_map', choropleth_map, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var bar_chart = "./bar_chart.vg.json";
+vegaEmbed('#bar_chart', bar_chart, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var alluvial_diagram = "./alluvial_diagram.vg.json";
+vegaEmbed('#alluvial_diagram', alluvial_diagram, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var bubble_plot = "./bubble_plot.vg.json";
+vegaEmbed('#bubble_plot', bubble_plot, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var connected_dot_plot = "./connected_dot_plot.vg.json";
+vegaEmbed('#connected_dot_plot', connected_dot_plot, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
